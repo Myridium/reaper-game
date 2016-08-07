f0ac9556a16042e5a04711ae03a750aa5dd60da0
Myridium <mggrewar@hotmail.com>
2016-08-07 17:29:04 +1000

    Removed unecessary imports.

--- src/reaper/entity/PelletCollection.java
+++ src/reaper/entity/PelletCollection.java
@@ -11,8 +11,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
 import reaper.CVector;
 import reaper.GLDrawHelper;
 
